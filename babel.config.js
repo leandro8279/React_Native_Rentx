64166753d82a3f627254e7e676424979c0d3f22b
leandro8279 <leandro8279@gmail.com>
2022-05-11 15:19:39 -0300

    Configured project.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,33 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+         root:".",
+         extensions:[
+           ".js",
+           ".jsx",
+           ".tsx",
+           ".ts",
+           ".android.js",
+           ".android.tsx",
+           ".ios.js",
+           ".ios.tsx"
+         ],
+          alias: {
+            "@assets": "./src/assets",
+            "@components":"./src/components",
+            "@global":"./src/global",
+            "@services":"./src/services",
+            "@screens":"./src/screens",
+            "@navigation":"./src/navigation",
+            "@utils":"./src/utils",
+          },
+        },
+      ],
+    ]
   };
 };
